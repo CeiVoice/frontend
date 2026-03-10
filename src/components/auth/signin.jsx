8f51656b817f96f37b5915c71791b57557f9fb4f
hamnuttapat1 <67011213@kmitl.ac.th>
2026-03-11 01:58:19 +0700

    update signin signup

--- src/components/auth/signin.jsx
+++ src/components/auth/signin.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
-import { IoMdArrowBack } from "react-icons/io";
 import API_BASE from '../../config/api';
 import frameLogo from '../../assets/Frame_6.png';
 function Signin({ onSuccess, onBack, onRegister }) {
@@ -32,8 +31,6 @@ function Signin({ onSuccess, onBack, onRegister }) {
             alert('Something went wrong. Please try again.');
         }
     }
-
-
     const handleGoogleSuccess = async (credentialResponse) => {
         try {
             const res = await fetch(`${API_BASE}/auth/signin/google`, {
@@ -53,23 +50,40 @@ function Signin({ onSuccess, onBack, onRegister }) {
             alert('Something went wrong. Please try again.');
         }
     };
-
     return (
-        <div className='flex justify-center items-center bg-linear-to-tl from-[#4377E5] to-[#BFCDE9] w-screen h-screen'>
-            <div className='bg-white shadow-lg p-6 sm:p-8 rounded-3xl w-full max-w-md sm:max-w-lg md:max-w-xl'>
-                <form onSubmit={handleSubmit} className='flex flex-col items-center gap-8 p-10 w-full'>
+        <div className='flex w-screen h-screen overflow-hidden'>
+            {/*Left */}
+            <div className='hidden md:flex w-2/3 h-full bg-[#000000] flex-col items-center justify-center px-10 relative overflow-hidden'>
+                {/* Gradient circle */}
+                <div className='absolute w-[500px] h-[500px] rounded-full bg-gradient-to-br from-[#e2e543] to-black opacity-60 blur-3xl'></div>
+                <div className='flex flex-row mb-5'>
+                    <div className='flex flex-row gap-2'>
+                        <p className='text-white text-3xl lg:text-[45px] font-bold text-center leading-tight'>Welcome to</p>
+                        <p className='text-white text-3xl lg:text-[45px] font-bold text-center leading-tight'>CEI</p>
+                    </div>
+                    <p className='text-white text-3xl lg:text-[45px] font-bold text-center leading-tight'>Voice!</p>
+                </div>
+                <p className='text-white text-sm lg:text-[18px] text-center leading-relaxed'>
+                    Our platform helps report issues, track ticket progress,
+                </p>
+                <p className='text-white text-sm lg:text-[18px] text-center leading-relaxed'>
+                    and collaborate with the support team to resolve problems quickly.
+                </p>
+            </div>
+            {/*Right sign in*/}
+            <div className='bg-[#ffffff] w-full md:w-1/3 h-full flex items-center justify-center overflow-y-auto'>
+                <form onSubmit={handleSubmit} className='flex flex-col items-center gap-6 px-8 py-10 w-full max-w-sm md:max-w-md'>
                     <img src={frameLogo} alt="Frame" className='w-auto h-auto' />
-                    <p className='self-start text-black text-2xl'>Sign in</p>
-                    <input className='block px-3 py-2 border border-black rounded-3xl w-full' type="email" name='Gmail' placeholder='Example@gmail.com' required />
+                    <p className='self-start text-black text-2xl font-semibold'>Sign in</p>
+                    <input className='block px-3 py-2 border border-black rounded-3xl w-full bg-white' type="email" name='Gmail' placeholder='Example@gmail.com' required />
                     <div className='relative w-full'>
                         <input
-                            className='block px-3 py-2 pr-10 border border-black rounded-3xl w-full'
+                            className='block px-3 py-2 pr-10 border border-black rounded-3xl w-full bg-white'
                             type={showPassword ? 'text' : 'password'}
                             name='Password'
                             placeholder='Password'
                             required
                         />
-
                         <button
                             type='button'
                             className='top-1/2 right-5 absolute text-black-600 text-sm -translate-y-1/2 cursor-pointer'
@@ -79,7 +93,7 @@ function Signin({ onSuccess, onBack, onRegister }) {
                         </button>
                     </div>
 
-                    <button type="submit" className='bg-[#4377E5] hover:bg-blue-700 rounded-3xl w-full h-10 text-white cursor-pointer'>Sign in</button>
+                    <button type="submit" className='bg-[#000000] hover:bg-[#7a7979] rounded-3xl w-full h-10 text-white cursor-pointer'>Sign in</button>
                     <div className='flex flex-row gap-2'>
                         <p>Don't have an account?</p>
                         <button
@@ -108,5 +122,4 @@ function Signin({ onSuccess, onBack, onRegister }) {
         </div>
     )
 }
-
 export default Signin
